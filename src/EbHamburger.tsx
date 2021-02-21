929615e54b0fb384187f24edacd125972395b55c
Edmond Yeo <edmond.yeo@gmail.com>
2021-02-21 15:39:39 +1100

    add on click handler

--- src/EbHamburger.tsx
+++ src/EbHamburger.tsx
@@ -1,21 +1,21 @@
 import "./EbHamburger.scss";
+import {useState} from "react";
 
 const EbHamburger = (): JSX.Element => {
-    const isOpen = false;
-    const renderIcon = () => {
-        if (!isOpen) {
-            return (
-                <>
-                    <span className="btn-line"></span>
-                    <span className="btn-line"></span>
-                    <span className="btn-line"></span>
-                </>
-            );
-        }
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onClickHandler = () => {
+        setIsOpen(!isOpen);
     };
+
     return (
-        <div className={isOpen ? "hamburger hide-for-desktop" : "hamburger close hide-for-desktop"}>
-            {renderIcon()}
+        <div
+            onClick={onClickHandler}
+            className={!isOpen ? "hamburger hide-for-desktop" : "hamburger close hide-for-desktop"}
+        >
+            <span className="btn-line"></span>
+            <span className="btn-line"></span>
+            <span className="btn-line"></span>
         </div>
     );
 };
