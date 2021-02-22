2446616ecae9895350b3ccf28e7acd689c11e785
Edmond Yeo <edmond.yeo@gmail.com>
2021-02-22 22:38:09 +1100

    modal

--- src/components/EbModal.tsx
+++ src/components/EbModal.tsx
@@ -1,7 +1,18 @@
+import {spawn} from "child_process";
 import "./EbModal.scss";
 
 const EbModal = (): JSX.Element => {
-    return <div className="modal">Hello World</div>;
+    const links = ["Home", "About", "Contact", "Blog", "Careers"];
+
+    return (
+        <div className="modal flex flex-jc-se flex-ai-ac">
+            {links.map((link, index) => (
+                <a href="/" key={index}>
+                    {link}
+                </a>
+            ))}
+        </div>
+    );
 };
 
 export default EbModal;
