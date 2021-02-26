17dc1c64f6f5ad7dbbd17376002f4bcd36932ccc
Edmond Yeo <edmond.yeo@gmail.com>
2021-02-26 17:19:57 +1100

    updated header

--- src/components/EbHeader.tsx
+++ src/components/EbHeader.tsx
@@ -1,39 +1,69 @@
-import {useState} from "react";
-import EbHamburger from "./EbHamburger";
-import EbCta from "./EbCta";
-import EbModal from "./EbModal";
+import {useState, useEffect} from "react";
 
 import "./EbHeader.scss";
 
 const EbHeader = (): JSX.Element => {
+    const links = ["Home", "About", "Contact", "Blog", "Careers"];
     const [showModal, setShowModal] = useState(false);
     const onClickHandler = () => {
-        return setShowModal(!showModal);
+        const overlay = document.querySelector(".overlay");
+
+        setShowModal(!showModal);
     };
 
-    const renderModal = () => {
-        return (
-            <>
-                <EbModal />
-            </>
-        );
-    };
+    useEffect(() => {
+        const overLay = document.querySelector(".overlay");
+        const menu = document.querySelector(".header__menu");
+        const body = document.querySelector("body");
+        const fadeIn = overLay?.classList.contains("fade-in");
+        if (showModal && !fadeIn) {
+            overLay?.classList.remove("fade-out");
+            overLay?.classList.add("fade-in");
+            menu?.classList.add("fade-in");
+            body?.classList.add("no-scroll");
+        } else if (!showModal && fadeIn) {
+            overLay?.classList.remove("fade-in");
+            overLay?.classList.add("fade-out");
+            menu?.classList.add("fade-out");
+            body?.classList.remove("no-scroll");
+        }
+        return;
+    }, [showModal]);
 
     return (
-        <header className="header">
-            <nav className="flex flex-jc-sb flex-ai-ac">
+        <header className={"header" + (showModal ? " open" : "")}>
+            <div className="overlay"></div>
+            <nav className="container flex flex-jc-sb flex-ai-ac">
                 <a href="/" className="header__logo">
                     <img src="../images/logo.svg"></img>
                 </a>
-                <div onClick={onClickHandler}>
-                    <EbHamburger showModal={showModal} />
+                <div onClick={onClickHandler} className={"header__toggle hide-for-desktop"}>
+                    <span className="btn-line"></span>
+                    <span className="btn-line"></span>
+                    <span className="btn-line"></span>
                 </div>
-                <EbCta />
+                <div className="header__links hide-for-mobile">
+                    {links.map((link, index) => (
+                        <a key={index} href="/">
+                            {link}
+                        </a>
+                    ))}
+                </div>
                 <a href="/" className="button header__cta hide-for-mobile">
                     Request Invite
                 </a>
             </nav>
-            {showModal && renderModal()}
+            <div
+                className={
+                    "header__menu container" + (!showModal ? " has-fade" : "") + " hide-for-desktop"
+                }
+            >
+                {links.map((link, index) => (
+                    <a key={index} href="/">
+                        {link}
+                    </a>
+                ))}
+            </div>
         </header>
     );
 };
